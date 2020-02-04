1e049ea359caa79bf4ea13bf90f412dcfba2f96a
Felipe Matos <flpmatosa@gmail.com>
2020-02-04 15:55:16 -0300

    Implement IoC and configure presentation layer

---
+++ CleanArch.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Info.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArch.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Application layer
+            #region ApplicationLayer
+            services.AddScoped<ICourseService, CourseService>();
+
+            #endregion
+
+            //Infra.Data layer
+            #region Infra.DataLayer
+            services.AddScoped<ICourseRepository, CourseRepository>();
+
+            #endregion
+        }
+    }
+}
