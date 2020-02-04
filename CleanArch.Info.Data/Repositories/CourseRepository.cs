42715f561f3e868d64e9a80108a6613ded879276
Felipe Matos <flpmatosa@gmail.com>
2020-02-04 15:20:58 -0300

    Implementing course repository and service

---
+++ CleanArch.Info.Data/Repositories/CourseRepository.cs
@@ -0,0 +1,22 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Info.Data.Context;
+using System.Collections.Generic;
+
+namespace CleanArch.Info.Data.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly UniversityDbContext _context;
+
+        public CourseRepository(UniversityDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _context.Courses;
+        }
+    }
+}
