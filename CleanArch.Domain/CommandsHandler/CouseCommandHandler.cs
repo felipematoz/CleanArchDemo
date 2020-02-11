bdf550cc0774e335c10c8198efb7e2fafcf07128
Felipe Matos <flpmatosa@gmail.com>
2020-02-11 13:46:22 -0300

    Adding Commands, Handlers, Updated Course Repository, Cleaned Up IoC container

---
+++ CleanArch.Domain/CommandsHandler/CouseCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using CleanArch.Domain.Commands;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.CommandsHandler
+{
+    public class CouseCommandHandler : IRequestHandler<CreateCourseCommand, bool>
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public CouseCommandHandler(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        public Task<bool> Handle(CreateCourseCommand request, CancellationToken cancellationToken)
+        {
+            var course = new Course()
+            {
+                Name = request.Name,
+                Description = request.Description,
+                ImageUrl = request.ImageUrl
+            };
+
+            _courseRepository.Add(course);
+
+            return Task.FromResult(true);
+        }
+    }
+}
--- CleanArch.Infra.IoC/DependencyContainer.cs
+++ CleanArch.Infra.IoC/DependencyContainer.cs
@@ -1,7 +1,13 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.Services;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.CommandsHandler;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
+using CleanArch.Info.Data.Context;
 using CleanArch.Info.Data.Repositories;
+using CleanArch.Infra.Bus;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CleanArch.Infra.IoC
@@ -10,16 +16,25 @@ namespace CleanArch.Infra.IoC
     {
         public static void RegisterServices(IServiceCollection services)
         {
+            //Domain InMemoryBus MediatR
+            #region MediatR
+            services.AddScoped<IMediatorHandler, InMemoryBus>();
+            #endregion
+
+            //Domain Handlers
+            #region Handlers
+            services.AddScoped<IRequestHandler<CreateCourseCommand, bool>, CouseCommandHandler>();
+            #endregion
+
             //Application layer
             #region ApplicationLayer
             services.AddScoped<ICourseService, CourseService>();
-
             #endregion
 
             //Infra.Data layer
             #region Infra.DataLayer
             services.AddScoped<ICourseRepository, CourseRepository>();
-
+            services.AddScoped<UniversityDbContext>();
             #endregion
         }
     }
