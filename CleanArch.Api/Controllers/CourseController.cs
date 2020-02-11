dc9fc320f04b827539e309811e5905353af7a7e7
Felipe Matos <flpmatosa@gmail.com>
2020-02-11 13:57:35 -0300

    Adding API project

---
+++ CleanArch.Api/Controllers/CourseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController: ControllerBase
+    {
+
+        
+    }
+}
